52ed986041422eed1f5a340b7fac3ae6c052916a
Ezra Nestel <ezranestel@gmail.com>
2021-06-29 19:38:54 -0700

    Set to logarithmic enhanced graph

--- TempCalc.py
+++ TempCalc.py
@@ -3,19 +3,23 @@ from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
 
 
-def exponential_fit(x, a, b, c):
-    return a * np.exp(-b * x) + c
+def heat_function(x, a, b, c):
+    temp = a * np.log(x + b) + c
+    #print(temp)
+    return temp
 
 
 if __name__ == "__main__":
-    x = np.array([0, 1, 2, 3, 4, 5])
-    y = np.array([30, 50, 80, 160, 300, 580])
-    fitting_parameters, covariance = curve_fit(exponential_fit, x, y)
+    x = np.array([0, 3, 5, 9])
+    y = np.array([30, 60, 84, 100])
+    fitting_parameters, pcov = curve_fit(heat_function, x, y)
     a, b, c = fitting_parameters
 
-    next_x = 6
-    next_y = exponential_fit(next_x, a, b, c)
+    next_x = 12
+    next_y = heat_function(next_x, a, b, c)
 
-    plt.plot(y)
-    plt.plot(np.append(y, next_y), 'ro')
+    plt.plot(x, y, 'r--')
+    plt.plot(np.append(x, next_x), np.append(y, next_y), 'ro')
+    plt.xlabel('Time (m)')
+    plt.ylabel('Temperature (C)')
     plt.show()
